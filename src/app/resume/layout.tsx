7c9f182c76a61eade20a12cbaea70ee4f42beb6f
Klink <85062+dogmar@users.noreply.github.com>
2025-12-10 11:34:27 -0800

    Upgrade nextjs

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
 import MainMenu from "../components/MainMenu";
+import styles from "./resume.module.scss";
 
-const ResumePage = ({children}:{children:ReactNode}) => (
-  <>
+const ResumePage = ({ children }: { children: ReactNode }) => (
+  <div className={styles.resumePage}>
     <MainMenu />
     {children}
-  </>
+  </div>
 );
 
 export default ResumePage;
