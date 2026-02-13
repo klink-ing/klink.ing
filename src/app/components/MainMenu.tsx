be55007f2c1a2380a133eeabb03296b1a4fc87fb
Klink <85062+dogmar@users.noreply.github.com>
2026-02-13 12:31:34 -0800

    Use biome

--- src/app/components/MainMenu.tsx
+++ src/app/components/MainMenu.tsx
@@ -3,12 +3,12 @@ import Logo from "./Logo";
 import styles from "./MainMenu.module.scss";
 
 const MainMenu = () => (
-  <nav className={styles.menu}>
-    <Link className={styles.logo} href="/">
-      <Logo />
-    </Link>
-    <Link href="/resume">Resumé</Link>
-  </nav>
+	<nav className={styles.menu}>
+		<Link className={styles.logo} href="/">
+			<Logo />
+		</Link>
+		<Link href="/resume">Resumé</Link>
+	</nav>
 );
 
 export default MainMenu;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-import styles from "./index.module.scss";
-import Logo from "./components/Logo";
 import Link from "next/link";
+import Logo from "./components/Logo";
+import styles from "./index.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.splash}>
-      <div className={styles.splashInner}>
-        <Logo className={styles.logo} />
-        <nav>
-          <ul>
-            <li>
-              <Link href="/resume">Resumé</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.splash}>
+			<div className={styles.splashInner}>
+				<Logo className={styles.logo} />
+				<nav>
+					<ul>
+						<li>
+							<Link href="/resume">Resumé</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
 }
